18ecd6b2cf14385412949490751eb3f23260a56c
dumptcp <28487422+dumptcp@users.noreply.github.com>
2026-09-19 09:08:27 +0000

    Update main.go

--- cache-all-no-compression/main.go
+++ cache-all-no-compression/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"hash/fnv"
 	"io/fs"
 	"log"
 	"mime"
@@ -40,8 +42,8 @@ const (
 )
 
 type file struct {
-	ctype, cc string
-	body      []byte
+	ctype, cc, etag string
+	body            []byte
 }
 
 var files = map[string]*file{}
@@ -84,7 +86,14 @@ func serve(c fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusNotFound)
 	}
 
+	if inm := c.Get("If-None-Match"); inm != "" && strings.Contains(inm, f.etag) {
+		c.Set("ETag", f.etag)
+		c.Set("Cache-Control", f.cc)
+		return c.SendStatus(fiber.StatusNotModified)
+	}
+
 	h := &c.Response().Header
+	h.Set("ETag", f.etag)
 	h.Set("Cache-Control", f.cc)
 	h.Set("X-Content-Type-Options", "nosniff")
 	h.Set("Referrer-Policy", "strict-origin-when-cross-origin")
@@ -161,7 +170,12 @@ func add(rel string, body []byte, ctype string) {
 			ctype = "application/wasm"
 		}
 	}
-	files[rel] = &file{ctype: ctype, cc: assetCacheControl, body: body}
+
+	h := fnv.New64a()
+	_, _ = h.Write(body)
+	etag := fmt.Sprintf(`"%016x"`, h.Sum64())
+
+	files[rel] = &file{ctype: ctype, cc: assetCacheControl, etag: etag, body: body}
 }
 
 func envStr(key, fallback string) string {
